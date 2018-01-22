d40c41bb82f5bdf28605ad29a22fbc56973fcedf
Scott <skepok1@gmail.com>
2018-01-22 22:32:27 +0000

    updates

---
+++ src/UmbracoImporter/UmbracoImporter/ConsoleSandbox/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UmbracoImporter;
+
+namespace ConsoleSandbox
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			YamlParser parser = new YamlParser();
+			parser.Load();
+		}
+	}
+}
---
+++ src/UmbracoImporter/UmbracoImporter/UmbracoImporter/YamlParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.RepresentationModel;
+
+namespace UmbracoImporter
+{
+    public class YamlParser
+    {
+		public void Load()
+		{
+			using (StreamReader reader = new StreamReader(@"C:\Data\Projects\UmbracoImporter\src\UmbracoImporter\UmbracoImporter\UmbracoImporter\mentor-site-spec.txt"))
+			{
+				// Load the stream
+				var yaml = new YamlStream();
+				yaml.Load(reader);
+
+				// Examine the stream
+				var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+				string s = string.Empty;
+				foreach (var entry in mapping.Children)
+				{
+					s += (((YamlScalarNode)entry.Key).Value);
+				}
+
+				// List all the items
+				var items = (YamlSequenceNode)mapping.Children[new YamlScalarNode("items")];
+				foreach (YamlMappingNode item in items)
+				{
+					//output.WriteLine(
+					//	"{0}\t{1}",
+					//	item.Children[new YamlScalarNode("part_no")],
+					//	item.Children[new YamlScalarNode("descrip")]
+					//);
+				}
+			}
+		}
+    }
+}
